26aa5a27a74fc9913844d30946e7ad93efaac09a
alluring-machine-4767 <bgreiser@gmail.com>
2020-08-16 13:44:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,13 @@ function destructivelyAddElementToBeginningOfArray(x, y) {
   x.unshift(y);
   return x;
 };
+
+function addElementToEndOfArray(x, y) {
+  let newArr = [x, ...y];
+  return newArr;
+};
+
+function destructivelyAddElementToEndOfArray(x, y) {
+  x.push(y);
+  return x;
+};
